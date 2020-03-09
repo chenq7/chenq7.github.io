7ac768c9df86267c039a8798bc6b11f42d5750e3
Qixiang <qixiang100@gmail.com>
2020-03-09 04:15:48 -0400

    finished styling portfolio pages

--- Readme.txt
+++ Readme.txt
@@ -4,3 +4,4 @@ Theme Name: Folio
 Theme URL: https://bootstrapmade.com/folio-bootstrap-portfolio-template/
 Author: BootstrapMade.com
 Author URL: https://bootstrapmade.com
+
--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Qixiang Chen.", "a Software Engineer.", "currently working on the projects listed below"],
       typeSpeed: 100,
       loop: true,
     });
